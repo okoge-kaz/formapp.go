a4c3bc76483f01fdc7c3fc882f4f5c49264ad446
kazuki <kaz.tokyo.tech20@gmail.com>
2022-10-18 10:47:06 +0900

    pass assignment3-1

--- main.go
+++ main.go
@@ -4,23 +4,33 @@ import (
 	"fmt"
 	"net/http"
 
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 // config
 const port = 8000
 
 func main() {
-    // initialize Gin engine
-    engine := gin.Default()
+	// initialize Gin engine
+	engine := gin.Default()
 
-    // routing
-    engine.GET("/", rootHandler)
+	// routing
+	engine.GET("/", rootHandler)
+	engine.GET("/bye", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Good Bye!")
+	})
+	engine.GET("/hello.jp", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "こんにちは")
+	})
+	engine.GET("/hello/:name", func(ctx *gin.Context) {
+		name := ctx.Param("name")
+		ctx.String(http.StatusOK, "Hello %s!", name)
+	})
 
-    // start server
-    engine.Run(fmt.Sprintf(":%d", port))
+	// start server
+	engine.Run(fmt.Sprintf(":%d", port))
 }
 
 func rootHandler(ctx *gin.Context) {
-    ctx.String(http.StatusOK, "Hello world.")
+	ctx.String(http.StatusOK, "Hello world.")
 }
